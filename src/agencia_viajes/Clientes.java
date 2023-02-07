3f0548f3b5e6e6379590268c020c00d854d06b2f
Gerardo Larrinaga <Gerardo Larrinaga@HZ321162.H012108.NET>
2023-02-07 08:48:49 +0100

    clientes habitaciones

---
+++ src/agencia_viajes/Clientes.java
@@ -0,0 +1,47 @@
+package agencia_viajes;
+
+public class Clientes {
+	private String 	dni;
+	private String	nombre;
+	private String apellidos;
+	private String localidad;
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public Clientes(String dni, String nombre, String apellidos, String localidad) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.localidad = localidad;
+	}
+	public Clientes() {
+	}
+	@Override
+	public String toString() {
+		return "Clientes [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", localidad=" + localidad
+				+ "]";
+	}
+	
+
+}
