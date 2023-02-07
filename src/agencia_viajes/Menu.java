3fa60202561028379a0fdb3835a59fd3c64be6b4
Gerardo Larrinaga <Gerardo Larrinaga@HZ321162.H012108.NET>
2023-02-07 09:07:21 +0100

    menu principal

---
+++ src/agencia_viajes/Menu.java
@@ -0,0 +1,18 @@
+package agencia_viajes;
+
+public class Menu {
+	
+	public static final int GESTIONAR_CLIENTES = 1;
+	public static final int GESTIONAR_HABITACIONES = 2;
+	public static final int GESTIONAR_HOTELES =3;
+	public static final int GESTIONAR_RESERVAS = 4;
+	
+	public static final int SALIR = 0;
+	
+	public static final int ALTA_CLIENTE=1;
+	public static final int BAJA_CLIENTE = 2;
+	public static final int MODIFICAR_CLIENTE = 3;
+	public static final int VER_CLIENTE = 4;
+	
+
+}
