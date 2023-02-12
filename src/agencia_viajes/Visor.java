c06e0856d6c3289418e0ddbe2929469ce1da859e
Usuario <Usuario@DESKTOP-AB3UK2C>
2023-02-12 21:26:16 +0100

    Visor

---
+++ src/agencia_viajes/Visor.java
@@ -0,0 +1,17 @@
+package agencia_viajes;
+
+import java.util.ArrayList;
+
+public class Visor {
+
+	
+	public static void mostrarClientes(ArrayList<Cliente>clientes) {
+	for (Cliente cliente : clientes) {
+		System.out.println(cliente);
+	}	
+	}
+	
+	public static void mostrarCliente(Cliente cliente) {
+		System.out.println(cliente);
+	}
+}
