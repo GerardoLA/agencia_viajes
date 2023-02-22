fa92a9a21798acec461002000dc28a2758db5946
GerardoLA <gerardolarrinaga@gmail.com>
2023-02-22 18:57:19 +0100

    Add mostrarHabitacion,mostrarHabitaciones,mostrarHotel y mostrarHoteles

--- src/agencia_viajes/Visor.java
+++ src/agencia_viajes/Visor.java
@@ -15,6 +15,30 @@ public class Visor {
 	public static void mostrarCliente(Cliente cliente) {
 		System.out.println(cliente);
 	}
+	
+	public static void mostrarHabitacion(Habitacion habitacion) {
+		System.out.println(habitacion);
+		
+	}
+	
+	public static void mostrarHabitaciones(ArrayList<Habitacion>ListaHabitaciones) {
+		for (Habitacion habitacion : ListaHabitaciones) {
+			System.out.println(habitacion);
+		}
+		
+	}
+	
+	public static void mostrarHotel(Hotel hotel) {
+		System.out.println(hotel);
+	}
+	
+	
+	public static void mostrarHoteles(ArrayList<Hotel>ListaHoteles) {
+		for (Hotel hotel : ListaHoteles) {
+			System.out.println(hotel);
+		}
+	}
+	
 }
 
 	
